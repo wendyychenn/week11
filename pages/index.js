fb87ba71bcd4a154dfde3251b4f970fb75025c04
wendyychenn <wchen242@my.bcit.ca>
2022-11-14 10:15:24 -0800

    files and folders

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function Home() {
+const [number, setNumber] = useState(0);
+
+  return (
+    <div>
+      <div>{number}</div>
+      <button onClick={() => setNumber(number + 1)}>increment</button>
+      <button onClick={() => setNumber(number - 1)}>decrement</button>
+    </div>
+  )
+}
